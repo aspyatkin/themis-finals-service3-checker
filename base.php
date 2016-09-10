d08d1effc3502fb170bdd6ae65ec010867084feb
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-09-10 09:09:47 +0000

    Respect LOG_LEVEL environment variable value

--- base.php
+++ base.php
@@ -35,9 +35,22 @@ $logger = null;
 
 function getLogger() {
     global $logger;
+
     if ($logger === null) {
+        $logLevel = Logger::INFO;
+        switch (getenv('LOG_LEVEL')) {
+            case 'DEBUG': $logLevel = Logger::DEBUG; break;
+            case 'INFO': $logLevel = Logger::INFO; break;
+            case 'NOTICE': $logLevel = Logger::NOTICE; break;
+            case 'WARNING': $logLevel = Logger::WARNING; break;
+            case 'ERROR': $logLevel = Logger::ERROR; break;
+            case 'CRITICAL': $logLevel = Logger::CRITICAL; break;
+            case 'ALERT': $logLevel = Logger::ALERT; break;
+            case 'EMERGENCY': $logLevel = Logger::EMERGENCY; break;
+        }
+
         $logger = new Logger('checker');
-        $logger->pushHandler(new StreamHandler('php://stdout'));
+        $logger->pushHandler(new StreamHandler('php://stdout', $logLevel));
     }
     return $logger;
 }
